c19ddb4ebb2bacabb8f572951e5a4800f6bf7042
Stephen Pride <spride@gmail.com>
2023-08-20 16:16:58 -0500

    Update main.tf

--- terraform-test/main.tf
+++ terraform-test/main.tf
@@ -28,15 +28,25 @@ locals {
   #    for sub in var.subscriptions: "${sub.protocol}-${sub.endpoint}" => sub.endpoint
   #    if sub.protocol == "lambda"
   #}
+  data = {
+      for sub in var.subscriptions: sub.endpoint => split(":",sub.endpoint)[6]
+      if sub.protocol == "lambda"
+  }
   #data = [
-  #    for sub in var.subscriptions: sub.endpoint
+  #    for sub in var.subscriptions: split(":",sub.endpoint)[6]
   #    if sub.protocol == "lambda"
   #]
-  data = [
-      for sub in var.subscriptions: split(":",sub.endpoint)[6]
-      if sub.protocol == "lambda"
-  ]
 }
+
+#resource "aws_lambda_permission" "with_sns" {
+#  for_each      = local.data
+
+#  statement_id  = "AllowExecutionFromSNS"
+#  action        = "lambda:InvokeFunction"
+#  function_name = each.value.
+#  principal     = "sns.amazonaws.com"
+#  source_arn    = aws_sns_topic.default.arn
+#}
 
 output stuff {
   value = local.data
